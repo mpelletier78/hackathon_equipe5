fceda0041e8cdd2f58d3ede48b1e98184d1a5fe2
Ruben MARTINS [55814b1f-fb5d-42c7-8b58-8e28ec9c513c] <ruben.lima-martins-ext@klx.pt>
2024-06-24 12:06:32 +0100

    extract model methods

--- backen/src/main/java/parsing/CSVToJsonConverter.java
+++ backen/src/main/java/parsing/CSVToJsonConverter.java
@@ -5,6 +5,7 @@ import com.opencsv.bean.CsvToBeanBuilder;
 import model.Athlete;
 import model.Competitor;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
@@ -12,22 +13,29 @@ import java.util.List;
 public class CSVToJsonConverter {
 
     public String convertAthletes(String csvFilePath) throws IOException {
-        List<Athlete> athletes = new CsvToBeanBuilder<Athlete>(new FileReader(csvFilePath))
-            .withType(Athlete.class)
-            .build()
-            .parse();
-
+        List<Athlete> athletes = mapToAthletes(csvFilePath);
         Gson gson = new Gson();
         return gson.toJson(athletes);
     }
 
-    public String convertCompetitors(String csvFilePath) throws IOException {
-        List<Competitor> competitors = new CsvToBeanBuilder<Competitor>(new FileReader(csvFilePath))
-            .withType(Competitor.class)
+    private static List<Athlete> mapToAthletes(String csvFilePath) throws FileNotFoundException {
+        return new CsvToBeanBuilder<Athlete>(new FileReader(csvFilePath))
+            .withType(Athlete.class)
             .build()
             .parse();
+    }
+
+    public String convertCompetitors(String csvFilePath) throws IOException {
+        List<Competitor> competitors = mapCompetitors(csvFilePath);
 
         Gson gson = new Gson();
         return gson.toJson(competitors);
+    }
+
+    private static List<Competitor> mapCompetitors(String csvFilePath) throws FileNotFoundException {
+        return new CsvToBeanBuilder<Competitor>(new FileReader(csvFilePath))
+            .withType(Competitor.class)
+            .build()
+            .parse();
     }
 }
