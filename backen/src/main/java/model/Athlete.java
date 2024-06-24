2ab0372b00ffed826b870727506b55571213a56a
Ruben MARTINS [55814b1f-fb5d-42c7-8b58-8e28ec9c513c] <ruben.lima-martins-ext@klx.pt>
2024-06-24 11:34:31 +0100

    csv parsing

---
+++ backen/src/main/java/model/Athlete.java
@@ -0,0 +1,76 @@
+package model;
+
+public class Athlete {
+    private String athlete;
+    private String sex;
+    private String country;
+    private String discipline;
+    private int year;
+    private int position;
+    private String time;
+    private String note;
+
+    public String getAthlete() {
+        return athlete;
+    }
+
+    public void setAthlete(String athlete) {
+        this.athlete = athlete;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getDiscipline() {
+        return discipline;
+    }
+
+    public void setDiscipline(String discipline) {
+        this.discipline = discipline;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
