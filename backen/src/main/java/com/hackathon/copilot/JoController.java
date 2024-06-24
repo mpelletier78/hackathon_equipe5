9d9855435f3a5b3352574f66b8676ea4a17f5918
Elhadi LEBCIRA [e0860bfb-7bc2-4df6-b8af-7e7580023f81] <Elhadi.LEBCIRA@ca-ps.com>
2024-06-24 12:35:09 +0200

    Ajout du 2 ressources

--- backen/src/main/java/com/hackathon/copilot/JoController.java
+++ backen/src/main/java/com/hackathon/copilot/JoController.java
@@ -1,6 +1,33 @@
 package com.hackathon.copilot;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
 public class JoController {
+
+    @GetMapping("/athletes")
+    public List<Athlete> getAthletes() {
+        return List.of(
+                new Athlete("Jo", "Doe", 25),
+                new Athlete("Jane", "Doe", 25)
+        );
+
+    // add rest ressource to expose array of json competitor
+        @GetMapping("/competitors")
+                public List<Competitor> getCompetitors() {
+                    return List.of(
+                            new Competitor("Jo", "Doe", 25, "France"),
+                            new Competitor("Jane", "Doe", 25, "France")
+                    );
+                }
+
+
 
 
 }
