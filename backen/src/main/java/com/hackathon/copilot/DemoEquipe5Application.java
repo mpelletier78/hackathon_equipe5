2581c24b72c7a2a060fb762ba3f2918c1459354c
Elhadi LEBCIRA [e0860bfb-7bc2-4df6-b8af-7e7580023f81] <Elhadi.LEBCIRA@ca-ps.com>
2024-06-24 12:22:38 +0200

    Ajout du socle pour rest controller

---
+++ backen/src/main/java/com/hackathon/copilot/DemoEquipe5Application.java
@@ -0,0 +1,13 @@
+package com.hackathon.copilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEquipe5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEquipe5Application.class, args);
+	}
+
+}
---
+++ backen/src/main/java/com/hackathon/copilot/JoController.java
@@ -0,0 +1,6 @@
+package com.hackathon.copilot;
+
+public class JoController {
+
+
+}
